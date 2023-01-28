5072d75c644667f0c6a1fb45d11629a91e419fe9
user <negiiinn.nm@gmail.com>
2023-01-28 11:15:24 +0330

    commit style updated

--- php/food_view.php
+++ php/food_view.php
@@ -109,13 +109,20 @@ if (isset($_POST['submit'])) {
                                 echo "<div class='container-fluid p-3'>";
                                 if (($result=mysqli_query($con,$comment_sql)))
                                 {
+                                    $count = 0;
                                     while($row1=mysqli_fetch_row($result))
                                     {
-                                        echo "<div class='rounded p-3 mb-3' style='background-color: #ffc554'><div class='d-flex justify-content-between align-items-center'><h3 class='card-title'>$row1[2]</h3>
+                                        if($count % 2 == 0){
+                                            $s = "commentL";
+                                        }else{
+                                            $s = "commentR";
+                                        }
+                                        echo "<div class='$s p-3 mb-3'><div class='d-flex justify-content-between align-items-center'><h3 class='card-title'>$row1[2]</h3>
                                         <div><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#fff4d4' class='bi bi-star-fill' viewBox='0 0 16 16'>
                                         <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z'/>
                                         </svg><strong> $row1[0]</strong></div></div>";
                                         echo "<p class='card-text'>$row1[1]</p></div>";
+                                        $count++;
                                     }
                                 }
                                 echo "<main class='form-signin w-400'>
